ae821a4ccc39aae368b1100a07521ae4616c600b
arielayb <cyberspace009@bitbucket.org>
2021-12-31 22:37:45 -0700

    added docker manager

--- Assets/Scripts/DockerManager.cs
+++ Assets/Scripts/DockerManager.cs
@@ -2,32 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Diagnostics;
+using System;
+using System.Text;
 
 public class DockerManager : MonoBehaviour
 {
     private ProcessStartInfo dockerInfo_;
     private static int numOutputLines = 0;
+    private static StringBuilder sortOutput = null;
 
     // Start is called before the first frame update
     void Start()
     {
+        dockerInfo_ = new ProcessStartInfo("cmd", "/c docker start gnss-sdr");
+        //dockerInfo_.Arguments = "docker run -a stdin -a stdout -it gnss-sdr";
         dockerInfo_.CreateNoWindow = true;
         dockerInfo_.UseShellExecute = false;
         dockerInfo_.RedirectStandardOutput = true;
         dockerInfo_.RedirectStandardError = true;
-    }
+        UnityEngine.Debug.Log("docker loaded");
 
-    public void GetSatelliteCoordinates(object sender, DataReceivedEventArgs e) {
-        if () { 
-        
-        }
-    
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
         Process process = new Process();
         process.StartInfo = dockerInfo_;
 
@@ -45,6 +39,27 @@ public class DockerManager : MonoBehaviour
 
         int exitCode = process.ExitCode;
         process.Close();
+    }
+
+    public void GetSatelliteCoordinates(object sender, 
+                                        DataReceivedEventArgs outLine) {
+        // Collect the sort command output.
+        if (!String.IsNullOrEmpty(outLine.Data))
+        {
+            UnityEngine.Debug.Log(numOutputLines);
+            numOutputLines++;
+
+            // Add the text to the collected output.
+            sortOutput.Append(Environment.NewLine +
+                $"[{numOutputLines}] - {outLine.Data}");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
 
     }
 }
